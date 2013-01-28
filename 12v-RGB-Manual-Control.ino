e43f1122d45fa142ee2b543caa634bcf6c0d7c10
totallyrad <ian.kawabata@gmail.com>
2013-01-28 11:07:01 -0800

    Update 12v-RGB-Manual-Control.ino

--- 12v-RGB-Manual-Control.ino
+++ 12v-RGB-Manual-Control.ino
@@ -1,16 +1,15 @@
-
-// Init the Pins used for PWM
+                                                    // Init the Pins used for PWM
 int redPin = 3;
 int greenPin = 5;
 int bluePin = 6;
 
 
-// Init the Pins used for 10K pots
+                                                    // Init the Pins used for 10K pots
 const int redPotPin = 0;
 const int greenPotPin = 1;
 const int bluePotPin = 2;
 
-// Init our Vars
+                                                    // Init our Vars
 int currentColorValueRed;
 int currentColorValueGreen;
 int currentColorValueBlue;
@@ -25,26 +24,26 @@ void setup()
 
 void loop()
 {
-// Read the voltage on each analog pin then scale down to 0-255 and inverting the value for common anode
+                                                    // Read the voltage on each analog pin then scale down to 0-255 and inverting the value for common anode
   currentColorValueRed = (254 - map( analogRead(redPotPin), 0, 1024, 0, 255 ) ); 
   currentColorValueBlue = (254 - map( analogRead(bluePotPin), 0, 1024, 0, 255 ) );
   currentColorValueGreen = (254 - map( analogRead(greenPotPin), 0, 1024, 0, 255 ) );
 
-// Write the color to each pin using PWM and the value gathered above
-  analogWrite(redPin, currentColorValueRed);
+
+  analogWrite(redPin, currentColorValueRed);        // Write the color to each pin using PWM and the value gathered above
   analogWrite(bluePin, currentColorValueBlue);
   analogWrite(greenPin, currentColorValueGreen);
-    delay(50); // wait a sec
-  analogWrite(redPin,0); 
+    delay(50);                                      // wait a little bit. I considered adding in another variable here so a 4th pot could be used to control rate
+  analogWrite(redPin,0);                            // Turn off all pins
   analogWrite(bluePin,0); 
   analogWrite(greenPin,0); 
   
-  Serial.print("values for are R, G, B: ");
-  Serial.print(currentColorValueRed, DEC);    // print the acceleration in the X axis
-  Serial.print(" ");       // prints a space between the numbers
-  Serial.print(currentColorValueBlue, DEC);    // print the acceleration in the Y axis
-  Serial.print(" ");       // prints a space between the numbers
-  Serial.println(currentColorValueGreen, DEC);  // print the acceleration in the Z axis
-  delay(100);              // wait 100ms for next reading
+  Serial.print("values for R, G, B: ");
+  Serial.print(currentColorValueRed, DEC);          // print the acceleration in the R axis
+  Serial.print(" ");                                // prints a space between the numbers
+  Serial.print(currentColorValueBlue, DEC);         // print the acceleration in the G axis
+  Serial.print(" ");                                // prints a space between the numbers
+  Serial.println(currentColorValueGreen, DEC);      // print the acceleration in the B axis
+  delay(100);                                       // wait 100ms for next reading
 
 }
